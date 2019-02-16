cc9d49f3aa869f3c398a74f39e39a503ae6b9c1b
Abdoulaye N'Diaye <ndiaye.iv@gmail.com>
2019-02-16 21:26:37 +0100

    Update webSite

--- js/custom.js
+++ js/custom.js
@@ -37,9 +37,36 @@
     const displayPublications = (publications) => {
         let publicationsHTML = '<h2>Publications : </h2>';
         publications.forEach(publication => {
-            publicationsHTML += '<p><b>' + publication.title + ': </b><br/>' + publication.description + ' <a href="'+ HREF +'pdf/'+ publication.pdf +'" target="_blank"><img style="height: 25px;" src="./images/pdf.svg" alt></a></p>'
+            publicationsHTML += '<p><b>' + publication.title + ': </b>'+
+            '<br/><a href="' + HREF + 'pdf/' + publication.pdf + '" target="_blank">' + publication.description + '<img class="icon" src="./images/pdf.svg" alt></a></p>'
         });
-        document.querySelector('div.menu3').innerHTML = publicationsHTML;
+        document.querySelector('div.publication').innerHTML = publicationsHTML;
+    }
+
+    const displayOrganisation = (organisation) => {
+        const board = organisation.board,
+            projectManager = organisation.projectManager,
+            scientificCommitee = organisation.scientificCommitee;
+        let organisationHTML = '<ul class="list-group list-group-flush">' +
+        '<li class="list-group-item"><h4 class="pt-2 m-2">Board</h4></li>';
+
+        board.forEach(element => {
+            organisationHTML += '<li class="list-group-item"><b>' + element.poste + '</b> : ' + element.name + '</li>'
+        });
+
+        organisationHTML += '<li class="list-group-item"><h4 class="pt-2 m-2">Project Manager</h4></li>'
+
+        projectManager.forEach(element => {
+            organisationHTML += '<li class="list-group-item">' + element + '</li>'
+        });
+
+
+        organisationHTML += '<li class="list-group-item"><h4 class="pt-2 m-2">Scientific Commitee</h4></li>';
+        scientificCommitee.forEach(element => {
+            organisationHTML += '<li class="list-group-item">' + element + '</li>'
+        });
+        organisationHTML += '</ul>';
+        document.querySelector('div.organisation').innerHTML += organisationHTML;
     }
 
     function loadJSON(link, callback) {
@@ -60,10 +87,14 @@
         addListener();
         navBarItems = document.querySelectorAll('.nav-item');
         menuItemsTab = generateMenuItemsTab();
-        //displayPublications(laodPublications());
-        loadJSON(HREF + 'json/publications.json',(response) => {
+
+        loadJSON(HREF + 'json/publications.json', (response) => {
             // Parse JSON string into object
             displayPublications(JSON.parse(response));
+        });
+        loadJSON(HREF + 'json/organisation.json', (response) => {
+            // Parse JSON string into object
+            displayOrganisation(JSON.parse(response));
         });
     };
 
