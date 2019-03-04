8e6c830ec42949e2b327ad61b2f500998c85aaff
Abdoulaye N'Diaye <ndiaye.iv@gmail.com>
2019-03-04 23:55:09 +0100

    update according to comments

--- js/custom.js
+++ js/custom.js
@@ -172,6 +172,7 @@
 
     const displayPublications = (publications) => {
         let publicationsHTML = '<h2>Publications : </h2>';
+        publications.reverse();
         publications.forEach(publication => {
             publicationsHTML += '<p><b>' + publication.title + ': </b>' +
                 '<br/><a href="' + HREF + 'pdf/' + publication.pdf + '" target="_blank">' + publication.description + '<img class="icon" src="./images/pdf.svg" alt></a></p>'
@@ -181,28 +182,26 @@
 
     const displayOrganisation = (organisation) => {
         const board = organisation.board,
-            projectManager = organisation.projectManager,
             scientificCommitee = organisation.scientificCommitee;
         let organisationHTML = '<ul class="list-group list-group-flush">' +
             '<li class="list-group-item"><h4 class="pt-2 m-2">Board</h4></li>';
 
-        board.forEach(element => {
+        !!board && board.forEach(element => {
             organisationHTML += '<li class="list-group-item"><b>' + element.poste + '</b> : ' + element.name + '</li>'
         });
+        organisationHTML += '</ul>';
 
-        organisationHTML += '<li class="list-group-item"><h4 class="pt-2 m-2">Project Manager</h4></li>'
-
-        projectManager.forEach(element => {
-            organisationHTML += '<li class="list-group-item">' + element + '</li>'
+        document.querySelectorAll('div.board').forEach (domElement => {
+            domElement.innerHTML = organisationHTML;
         });
 
-
-        organisationHTML += '<li class="list-group-item"><h4 class="pt-2 m-2">Scientific Commitee</h4></li>';
-        scientificCommitee.forEach(element => {
+        organisationHTML = '<ul class="list-group list-group-flush">' +
+        '<li class="list-group-item"><h4 class="pt-2 m-2">Scientific Commitee</h4></li>';
+        !!scientificCommitee && scientificCommitee.forEach(element => {
             organisationHTML += '<li class="list-group-item">' + element + '</li>'
         });
         organisationHTML += '</ul>';
-        document.querySelector('div.organisation').innerHTML += organisationHTML;
+        document.querySelector('div.scientific').innerHTML += organisationHTML;
     }
 
     function loadJSON(link, callback) {
